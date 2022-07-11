e92e0f08da99244a645c187bae7e2e4c9930a989
Makeev-AC19-04 <wane2001@gmail.com>
2022-07-11 16:46:39 +0300

    Система опроса, обращение к пользователю

--- main.py
+++ main.py
@@ -6,39 +6,30 @@ import sql_requests
 token = '5570755905:AAHG6lllgMm8rOBHrm2sLHN9pp487BZh_Mk' #токен бота
 bot = telebot.TeleBot(token)
 
-class User(): # Класс пользователя
-    def __init__(self):
-        self.setName=False
-        self.name = ''
-        self.stopped=False
-        self.doingSurvey=False
-
-users = {} # Cловарь пользователей
-
-def CheckUser(): # Функция проверки на право пользователя использовать бота
-    pass
-
-def main(message):
-    global users
+def main(message): # Главная функция, отвечающая за отправку всех сообщений
     block_1(message)
-    while users[message.from_user.id].stopped:
-        time.sleep(1)
-#if action.stopped == False:
-    bot.send_message(message.chat.id, 'Следующая функция')
+    while sql_requests.get_status(message.from_user.username) == 'naming':
+        time.sleep(5)
+    bot.send_message(message.chat.id, 'Следующее сообщение')
     time.sleep(5)
     bot.send_message(message.chat.id, 'Твой тг-id: ' + str(message.from_user.id))
+    time.sleep(5)
+    ask_survey_1(message)
+    while sql_requests.get_status(message.from_user.username) == 'waiting':
+        time.sleep(5)
+    if sql_requests.get_status(message.from_user.username) == 'survey 1.1':
+        survey_1(message.chat.id, message.from_user.username)
+    bot.send_message(message.chat.id, 'Сообщение после опроса')
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
-    global users
     role = sql_requests.define_role(message) # Определяем роль
     if role == 'HR':
         bot.send_message(message.chat.id, 'Вы HR, добавьте сотрудника с помощью '
                                           'специальной команды')
     elif role == 'user':
-        if sql_requests.get_status(message) == 'none':
-            sql_requests.set_status(message, 'started')
-            users[message.from_user.id]=User()
+        if sql_requests.get_status(message.from_user.username) == 'none':
+            sql_requests.set_status(message.from_user.username, 'started')
             bot.send_message(message.chat.id,
                              "Привет!\nЯ - <b>{1.first_name}</b>, создан помочь тебе адаптироваться в нашей компании!".format(
                                  message.from_user, bot.get_me()),
@@ -48,49 +39,52 @@ def welcome(message):
             bot.send_message(message.chat.id, 'Бот уже запущен')
     else:
         bot.send_message(message.chat.id, 'Простите, у вас нет права'
-                                          'пользоваться этим ботом')
+                                          ' пользоваться этим ботом')
 
 @bot.message_handler(commands=['setname'])
 def block_1(message):
-    global users
+    sql_requests.set_status(message.from_user.username, 'naming')
     markup = types.InlineKeyboardMarkup()
     item1 = types.InlineKeyboardButton("Как в телеграмме", callback_data='as in telegram')
     item2 = types.InlineKeyboardButton("Ввести новое", callback_data='new name')
     markup.row(item1, item2)
     bot.send_message(message.chat.id, 'Как я смогу к тебе обращаться?', reply_markup=markup)
-    users[message.from_user.id].stopped=True
 
 @bot.message_handler(commands=['survey'])
-def survey_1(message):
-    global users
+def ask_survey_1(message): # Спрашиваем, желает ли пользователь пройти опрос
+    sql_requests.set_status(message.from_user.username, 'waiting')
     markup = types.InlineKeyboardMarkup()
     item1 = types.InlineKeyboardButton("Начать", callback_data='start survey')
     item2 = types.InlineKeyboardButton("Спасибо, нет", callback_data='cancel survey')
     markup.row(item1, item2)
-    bot.send_message(message.chat.id, 'Поздравляем с первым рабочим днем в нашей'
-                                      'компании! Пожалуйста, пройдите опрос', reply_markup=markup)
-    while users[message.from_user.id].stopped:
-        time.sleep(1)
-    bot.send_message(message.chat.id, 'Спасибо за ответы')
+    name = sql_requests.get_name(message.from_user.username)
+    bot.send_message(message.chat.id, name + ', поздравляем с твоим первым рабочим днем в нашей '
+                                      'компании! Пожалуйста, поделись своими впечатлениями', reply_markup=markup)
 
-def start_survey(message):
-    sur_1={'Как вам первый день?'}
+def survey_1(chat_id, user):
+    questions = {'1': 'Пример первого вопроса',
+                 '2': 'Пример второго вопроса',
+                 '3': 'Пример третьего вопроса',
+                 '4': 'Пример четвертого вопроса'}
+    for i in questions:
+        bot.send_message(chat_id, questions[i]) # Отправляем нужный вопрос
+        #sql_requests.set_status(message.from_user.username, 'survey 1.' + i) # Задаем статус с номером вопроса
+        while sql_requests.get_status(user) == 'survey 1.' + i: # Ждем пока не получим ответа на текущий вопрос
+            time.sleep(2)
+    bot.send_message(chat_id, "Спасибо за участие!")
 
 def add_hr(message): # Добавление hr в БД
     response = sql_requests.add_hr(message)
     bot.send_message(chat_id=message.chat.id, text=response)
 
 def set_name(message):
-    global users
-    users[message.from_user.id].name = message.text
     sql_requests.set_name(message.text, message.from_user.username)
-    message_set_name = 'Приятно познакомиться, ' + users[message.from_user.id].name + '!\nДля редактирования имени введи /setname'
+    message_set_name = 'Приятно познакомиться, ' + message.text + '!\nДля редактирования имени введи /setname'
     bot.send_message(chat_id=message.chat.id, text=message_set_name)
-    users[message.from_user.id].setName = False
-    users[message.from_user.id].stopped = False
+    sql_requests.set_status(message.from_user.username, 'going')
 
 def add_user(message):
-    response = '';
+    response = ''
     if sql_requests.check_hr(message) == 1: # Формирование ответа в зависимости от того является ли человек эйчаром
         response = sql_requests.add_user(message)
     else:
@@ -100,7 +94,6 @@ def add_user(message):
 
 @bot.message_handler(content_types=['text'])
 def message_handler(message):
-    global users
     role = sql_requests.define_role(message)
     if role == 'HR':
         if '/>:swgPDGq:3Ce' in message.text:
@@ -109,32 +102,43 @@ def message_handler(message):
         if 'V<S<=2hjr/ptgQ=' in message.text:
             add_hr(message)
     elif role == 'user':
-        set_name(message)
-    #if users[message.from_user.id].setName == True:
-     #   set_name(message)
-
+        status = sql_requests.get_status(message.from_user.username)
+        if status == 'naming':
+            set_name(message)
+        elif 'survey' in status:
+            if status == 'survey 1.1':
+                sql_requests.add_survey(message.from_user.username, '1', message.text)
+                sql_requests.set_status(message.from_user.username, 'survey 1.2')
+            if status == 'survey 1.2':
+                sql_requests.set_survey(message.from_user.username, '1', '2', message.text)
+                sql_requests.set_status(message.from_user.username, 'survey 1.3')
+            if status == 'survey 1.3':
+                sql_requests.set_survey(message.from_user.username, '1', '3', message.text)
+                sql_requests.set_status(message.from_user.username, 'survey 1.4')
+            if status == 'survey 1.4':
+                sql_requests.set_survey(message.from_user.username, '1', '4', message.text)
+                sql_requests.set_status(message.from_user.username, 'survey 2')
+        else:
+            bot.send_message(message.chat.id, 'Пора пройти опрос')
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
-    global users
     message_set_name = ''
     if call.message:
         if call.data == 'as in telegram':
-            users[call.from_user.id].name=call.from_user.first_name
             sql_requests.set_name(call.from_user.first_name, call.from_user.username)
-            message_set_name = 'Приятно познакомиться, ' + users[call.from_user.id].name+ '!\nДля редактирования имени введи /setname'
-            users[call.from_user.id].stopped = False
+            sql_requests.set_status(call.from_user.username, 'going')
+            message_set_name = 'Приятно познакомиться, ' + call.from_user.first_name + '!\nДля редактирования имени введи /setname'
         elif call.data == 'new name':
             message_set_name = 'Введи новое имя'
-            users[call.from_user.id].setName = True
+            sql_requests.set_status(call.from_user.username, 'naming')
         elif call.data == 'start survey':
             message_set_name = 'Начинаем опрос'
-            users[call.from_user.id].stopped = False
-            users[call.from_user.id].doingSurvey = True
+            sql_requests.set_status(call.from_user.username, 'survey 1.1')
         elif call.data == 'cancel survey':
-            users[call.from_user.id].stopped = False
             message_set_name = 'Отменяем опрос'
+            sql_requests.set_status(call.from_user.username, 'survey 2')
         # remove inline buttons
         bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=message_set_name.format(call.message.from_user, bot.get_me()),
                               parse_mode='html',reply_markup=None)
---
+++ venv/sql_requests.py
@@ -0,0 +1,145 @@
+from mysql.connector import connect, Error
+import re
+
+def splitMessage(msg):
+    tosql = ''
+    for i in msg:
+        if i != '':
+            tosql += "'" + str(i) + "',"
+
+    return tosql[:-1]
+
+def add_hr(message): # Добавление эйчара в базу данных
+    split_msg = re.split(",|:|V<S<=2hjr/ptgQ=", message.text)
+    response = ''
+    tosql = "'@" + str(message.from_user.username) + "'," + splitMessage(split_msg)
+    with connect(host="localhost", user="root", password="54321") as connection:
+        addtobdcommand = 'INSERT INTO botdb.hr ' \
+                         '(tg_Name_HR,HR_name,recruiter_mail,Phone,_id_Subd_hr) ' \
+                         'VALUES (' + tosql + ");"
+        with connection.cursor() as cursor:
+            try:
+                cursor.execute(addtobdcommand)
+                connection.commit()  # Подтверждение изменений в БД
+            except Error as error:
+                response = 'Произошла ошибка, проверьте правильность введенных данных:\n' + str(error)
+            else:
+                response = 'Запись добавлена'
+    return response
+
+def check_hr(message): # Проверка, является ли данный пользователь эйчаром
+    with connect(host="localhost", user="root", password="54321") as connection:
+        checkHR = 'SELECT EXISTS(SELECT * FROM botdb.hr ' \
+                         "WHERE tg_Name_HR = '@" + str(message.from_user.username) + "');"
+        with connection.cursor() as cursor:
+            cursor.execute(checkHR) # Проверка, является ли тот кто добавляет человека эйчаром
+            return cursor.fetchall()[0][0]
+
+def add_user(message): # Добавление пользователя в базу данных
+    tg_name = message.text.replace('/>:swgPDGq:3Ce ', '')
+    response = str(tg_name)
+    with connect(host="localhost", user="root", password="54321") as connection:
+        addUser = "INSERT INTO botdb.user (tg_Name, key_id_subd, id_HR, status) \
+                VALUES ('" + tg_name + "'," + "(select _id_Subd_hr from botdb.hr where tg_Name_HR = '@" + message.from_user.username + \
+                  "'),'@" + message.from_user.username + "','none');"
+        with connection.cursor() as cursor:
+            try:
+                cursor.execute(addUser)
+                connection.commit()  # Подтверждение изменений в БД
+            except Error as error:
+                response = 'Произошла ошибка, проверьте правильность введенных данных:\n' + str(error)
+            else:
+                response = 'Запись добавлена'
+    return response
+
+def define_role(message): # Определение роли пользователя
+    if check_hr(message) == 1:
+        return 'HR'
+    with connect(
+            host="localhost",
+            user="root",
+            password="54321",
+    ) as connection:
+        checkHR = 'SELECT EXISTS(SELECT * FROM botdb.user ' \
+                  "WHERE tg_Name = '@" + str(message.from_user.username) + "');"
+        with connection.cursor() as cursor:
+            cursor.execute(checkHR)  # Проверка, является ли тот кто добавляет человека эйчаром
+            if cursor.fetchall()[0][0] == 1:
+                return 'user'
+            else:
+                return 'guest'
+
+def set_name(name, username): # Изменение имени пользователя в БД
+    with connect(
+            host="localhost",
+            user="root",
+            password="54321",
+    ) as connection:
+        setname = "UPDATE botdb.user SET user_Name = '" +\
+                  name + "' WHERE tg_Name = '@" + username + "';"
+        print(setname)
+        with connection.cursor() as cursor:
+            cursor.execute(setname)
+            connection.commit()
+
+def get_name(username): # Получить имя пользователя
+    with connect(
+            host="localhost",
+            user="root",
+            password="54321",
+    ) as connection:
+        getname = "SELECT user_Name FROM botdb.user WHERE tg_Name = '@" + username + "';"
+        print(getname)
+        with connection.cursor() as cursor:
+            cursor.execute(getname)
+            return cursor.fetchall()[0][0]
+
+def get_status(username): # Получить статус пользователя
+    with connect(host="localhost",
+                 user="root",
+                 password="54321") as connection:
+        status = "SELECT status FROM botdb.user WHERE tg_Name = '@" + \
+            username + "';"
+        print(status)
+        with connection.cursor() as cursor:
+            cursor.execute(status)
+            return cursor.fetchall()[0][0]
+
+def set_status(user, status): # Установить статус пользователя
+    with connect(
+            host="localhost",
+            user="root",
+            password="54321",
+    ) as connection:
+        setstatus = "UPDATE botdb.user SET status = '" + status + "'" +\
+                   " WHERE tg_Name = '@" + user + "';"
+        print(setstatus)
+        with connection.cursor() as cursor:
+            cursor.execute(setstatus)
+            connection.commit()
+
+def add_survey(user, survey_num, answer):
+    with connect(
+            host="localhost",
+            user="root",
+            password="54321",
+    ) as connection:
+        add_survey_to_sql = "INSERT INTO botdb.survey_" + survey_num + " (tg_Name, question_1) " \
+                             "VALUES ('@" + user + "','" + answer + "');"
+        print(add_survey_to_sql)
+        with connection.cursor() as cursor:
+            cursor.execute(add_survey_to_sql)
+            connection.commit()
+
+def set_survey(user, survey_num, question_num, answer):
+    with connect(
+            host="localhost",
+            user="root",
+            password="54321",
+    ) as connection:
+        set_answer = "UPDATE botdb.survey_" + survey_num + " SET question_" + question_num + "='" + \
+            answer + "' where tg_Name = '@" + user + "';"
+        print(set_answer)
+        with connection.cursor() as cursor:
+            cursor.execute(set_answer)
+            connection.commit()
